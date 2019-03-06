f26e114613de630f5d14c84af7e3fc64f048c312
lawwy <522031187@qq.com>
2019-03-07 00:14:55 +0800

    go mod

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"genetest/utils"
 )
 
 func main() {
-	p := NewPlayer()
-	g := NewGround()
-	h := &Host{}
-	env := g.GetAround(p.Pos)
-	m := p.NextMove(env)
-	success,np := g.Exec(p.Pos,m)
-	p.Pos = np
-	h.Count(m,success)
+	p := utils.NewPlayer()
+	g := utils.NewGround()
+	h := &utils.Host{}
+	for i:=0;i<1000;i++{
+		env := g.GetAround(p.Pos)
+		m := p.NextMove(env)
+		success,np := g.Exec(p.Pos,m)
+		p.Pos = np
+		h.Count(m,success)
+	}
+	//BUG:ALWAYS BE 0
 	fmt.Println(h.Score)
 }
